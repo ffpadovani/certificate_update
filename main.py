b6663c14dfa67aa0f1e380b98b106989bbba298c
ffpadovani <ffpadovani@outlook.com>
2023-05-14 01:26:07 -0300

    autopep8 aplicado

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from logging.handlers import RotatingFileHandler
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 
+
 def download(url: str, dest_path: str):
     filename = url.split('/')[-1].replace(" ", "_")
     file_path = os.path.join(dest_path, filename)
@@ -22,25 +23,33 @@ def download(url: str, dest_path: str):
     else:  # HTTP status code 4XX/5XX
         logger.error({"status_code": r.status_code, "message": r.text})
 
+
 def get_cer_info(cert: str):
-    certDecoded = x509.load_pem_x509_certificate(str.encode(cert), default_backend())
+    certDecoded = x509.load_pem_x509_certificate(
+        str.encode(cert), default_backend())
     print(certDecoded)
+
+
+def restart_service(service_name: str):
+    logger.info(f"Reiniciando serviço '{service_name}'")
 
 
 def main():
     path = "/etc/ssl/certs"
     url = "https://i216.ffpadovani.tech"
     cer_files = {"cert": "fullchain.pem", "key": "privkey.pem"}
-    service_name = sys.argv[0]
+    service_name = sys.argv[1]
 
     logger.info("####  Starting certificate updater  ####")
     logger.debug(f"Loadins files in '{path}' from verification.")
     # Listing all SSL certificates installed on the server
     local_cert_list = os.listdir(path=path)
 
-    logger.debug("Verifying that the files 'fullchain.pem' and 'privkey.pem' exist")
+    logger.debug(
+        "Verifying that the files 'fullchain.pem' and 'privkey.pem' exist")
     # Verifying that SSL certificate exist on the server
-    check_files = dict(map(lambda x: (x, True) if x in local_cert_list else (x, False), cer_files.values()))
+    check_files = dict(map(lambda x: (x, True) if x in local_cert_list else (
+        x, False), cer_files.values()))
 
     check_download = False
 
@@ -50,51 +59,60 @@ def main():
             logger.info(f"{path}/{k} not found")
             check_download = True
             logger.debug({"check_download": check_download})
-    
+
     # If the 'check_download' variable is True, start downloading the SSL certificate.
     if check_download:
         logger.debug("Starting to download the SSL Certificate files.")
-        [download(url=f"{url}/{filename}", dest_path=path) for filename in cer_files.values()]
+        [download(url=f"{url}/{filename}", dest_path=path)
+         for filename in cer_files.values()]
+        if service_name:
+            restart_service(service_name=service_name)
         return
-    
+
     # Loadind the current certificate to compare expiration date with ssl certificate in the server
     try:
         logger.info('Loading current certificate to verification')
-        current_cert = x509.load_pem_x509_certificate(open(f"{path}/{cer_files['cert']}", "rb").read(), default_backend())
+        current_cert = x509.load_pem_x509_certificate(
+            open(f"{path}/{cer_files['cert']}", "rb").read(), default_backend())
     except ValueError as e:
         logger.error(str(e))
         logger.debug("Starting to download the SSL Certificate files.")
-        [download(url=f"{url}/{filename}", dest_path=path) for filename in cer_files.values()]
+        [download(url=f"{url}/{filename}", dest_path=path)
+         for filename in cer_files.values()]
+        if service_name:
+            restart_service(service_name=service_name)
         return
-    
+
     try:
         logger.info('Loading SSL certificate from server for verification')
         r = requests.get(f"{url}/{cer_files['cert']}", stream=True)
         if not r.ok:
             logger.error({"status_code": r.status_code, "message": r.text})
             return
-        new_cert = x509.load_pem_x509_certificate(r.text.encode(), default_backend())
+        new_cert = x509.load_pem_x509_certificate(
+            r.text.encode(), default_backend())
     except Exception as e:
         logger.error(str(e))
 
     if current_cert.not_valid_after <= new_cert.not_valid_after:
         logger.info("Current SSL Certificate is valid")
         return
-    
-    logger.info("Updating SSL Certificate from this server") 
-    [download(url=f"{url}/{filename}", dest_path=path) for filename in cer_files.values()]
 
+    logger.info("Updating SSL Certificate from this server")
+    [download(url=f"{url}/{filename}", dest_path=path)
+     for filename in cer_files.values()]
     if service_name:
-        logger.info(f"Restarting service '{service_name}'")
-        print(service_name)
+        restart_service(service_name=service_name)
+
 
 if __name__ == "__main__":
     logging.basicConfig(
-        handlers=[RotatingFileHandler('./log/certificate_updater.log', maxBytes=100000, backupCount=10)],
+        handlers=[RotatingFileHandler(
+            './log/certificate_updater.log', maxBytes=100000, backupCount=10)],
         level=logging.DEBUG,
         format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
         datefmt='%Y-%m-%dT%H:%M:%S')
-    
+
     logger = logging.getLogger(__name__)
-    
+
     main()
